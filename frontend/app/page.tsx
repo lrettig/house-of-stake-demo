a61687000a102300ac538b7079ed07469c6457e6
Lane Rettig <lanerettig@gmail.com>
2025-02-07 16:10:19 -0800

    Frontend scaffold

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">House of Stake</h1>
+      <p className="text-xl">Vote-escrowed NEAR (veNEAR) implementation</p>
+    </main>
+  );
+} 
